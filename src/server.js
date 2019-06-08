5368ec93f7900ff4b4c52a14ee4680c9322fcabd
oliverjam <oliverjamesphillips@gmail.com>
2019-06-08 14:34:24 +0100

    Specify port in env variable

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const server = createServer((request, response) => {
   response.end(`<h1>Hello world</h1>`);
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+module.exports = server;
