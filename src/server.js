e7f0c0c6961e4142c724be43ba2e0d3f28db9941
oliverjam <oliverjamesphillips@gmail.com>
2019-06-08 16:29:40 +0100

    ESLint ignore server console.log

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const server = createServer((request, response) => {
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://localhost:3000"); // eslint-disable-line no-console
 });
 
 module.exports = server;
